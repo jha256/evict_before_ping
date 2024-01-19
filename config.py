1da44ec4248b5418d0e5f59ebb3ec1b9901ea4c6
jha256 <148934366+jha256@users.noreply.github.com>
2024-01-19 13:51:42 +0900

    aws instances config

---
+++ config.py
@@ -0,0 +1,24 @@
+config_seoul = {
+    'IP': "10.0.0.1",
+    'Key': "PEM/rtt_seoul.pem" 
+}
+
+config_cali = {
+    'IP': "10.0.0.2",
+    'Key': "PEM/rtt_cali.pem"
+}
+
+config_virginia = {
+    'IP': "10.0.0.3",
+    'Key': "PEM/rtt_virginia.pem"
+}
+
+config_frankfurt = {
+    'IP': "10.0.0.4",
+    'Key': "PEM/rtt_frankfurt.pem"
+}
+
+config_paulo = {
+    'IP': "10.0.0.5",
+    'Key': "PEM/rtt_paulo.pem" 
+}
